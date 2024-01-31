0ce8171712817b5d040cc4e2594204afa64ad380
kaushaltheguru <150440583+kaushaltheguru@users.noreply.github.com>
2024-01-31 15:53:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 function btnclick(){
     let p1 = document.getElementById("p1-el")
     console.log(p1)
-    p1.textContent = p1.textContent + " and looks like this button works"
+    let rand = math.rand();
+    p1.textContent =  rand + ", "
 }
