4bba2e1af261c1128c41f7eb21607104ea3edfc7
kaushaltheguru <150440583+kaushaltheguru@users.noreply.github.com>
2024-01-31 16:15:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function btnclick(){
     let p1 = document.getElementById("p1-el")
     console.log(p1)
-    let rand = math.rand();
-    p1.textContent =  rand + ", "
+    let rand = Math.random()*6
+    let num2 = Math.floor(rand)+1
+    p1.textContent =  rand + "   -   " + num2
 }
