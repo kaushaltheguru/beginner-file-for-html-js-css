beffc870844ef2795710bd792736127e1562cf1d
kaushaltheguru <150440583+kaushaltheguru@users.noreply.github.com>
2024-02-07 23:28:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 
-let person = 
-[
-    age = 12
-]
-console.log(person.age)
+let person = {
+    firstName: "rahul",
+    lastName: "aggrawal",
+    character: "bad",
+    age: 52,
+    sn:"",
+    printPersonDetail: function(){
+      let s = "My name is "+ this.firstName + " " + this.lastName + " and my character is very " + this.character
+      return s; 
+    },
+    setName: function(s){
+        this.lastName = s+" "+this.lastName;
+    }
+
+    }
+
+
+console.log(person[0])
 function btnclick(){
     let p1 = document.getElementById("p1-el")
     console.log(p1)
-    p1.textContent = p1.textContent + " and looks like this button works"
+    person.setName("donkey");
+    p1.textContent = p1.textContent + " and looks like this button works" + person.printPersonDetail();
 }
