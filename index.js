d9cf47b585934b07f0efaa3bc4a54a9b83485f8d
kaushaltheguru <150440583+kaushaltheguru@users.noreply.github.com>
2024-02-16 19:33:39 +0530

    new line

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+let countries = ["china","india","usa","indonesia","brazil"]
+
 
 function btnclick(){
     let p1 = document.getElementById("p1-el")
     console.log(p1)
-    p1.textContent = p1.textContent + " and looks like this button works"
+    p1.textContent = "The Most Populus Countries are as below"
+    for(i=0; i<countries.length; i++)
+    {
+    p1.innerHTML = "<br/>"    
+    p1.textContent = p1.textContent + countries[i]
+    }
 }
