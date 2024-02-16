872a28de3bccb12c114a4c4edd2f232dd4f1570a
kaushaltheguru <150440583+kaushaltheguru@users.noreply.github.com>
2024-02-16 23:23:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ let countries = ["china","india","usa","indonesia","brazil"]
 function btnclick(){
     let p1 = document.getElementById("p1-el")
     console.log(p1)
-    p1.textContent = "The Most Populus Countries are as below"
+    p1.innerHTML = "The Most Populus Countries are as below<br>"
     for(i=0; i<countries.length; i++)
-    {
-    p1.innerHTML = "<br/>"    
-    p1.textContent = p1.textContent + countries[i]
+    {    
+    p1.innerHTML += "<br>"+countries[i] + "<br>"
     }
 }
