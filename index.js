ff63b86f60ecff9aad21f56fb05e1165f72068ae
kaushaltheguru <150440583+kaushaltheguru@users.noreply.github.com>
2024-02-20 11:42:33 +0530

    elseif

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 
-let ta = document.getElementById("ta-el");
-console.log(ta);
+let countries = ["china","india","usa","indonesia","brazil"]
 function btnclick(){
     let p1 = document.getElementById("p1-el")
-    //console.log(p1)
-    //p1.textContent = p1.textContent + " and looks like this button works"
-    console.log(ta)
-    let age = ta.value;
-    let discount = "";
-    //p1.textContent += "-- "+ age + " --";
-    if(age<6)
+    p1.textContent = "";
+    for(let i=0; i<countries.length; i++)
     {
-        discount = "free"
-    }else if(age<17)
-    {
-        discount = "Child Discount"
-    }else if(age<26)
-    {
-        discount = "Student Discount"
-    }else if(age<66)
-    {
-        discount = "full price -- you have to instead pay for every one else's discount"
-    }else
-    {
-        discount = "senior Citizen"
+        p1.textContent += `Country with Rank ${i+1} in population is ${countries[i]}    `;
+    
     }
-    p1.textContent = "You are Eligible for: " + discount;
     
 }
